8478af8b7704d51facee1fe5b426154b5e92fbf0
Sayantan Mondal <62999423+MondalSayantan@users.noreply.github.com>
2024-07-15 16:36:27 +0530

    feat: remove code (#24)

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Person } from "./Person/Person";
-export { default as Footer } from "./Footer/Footer";
-export { default as CarouselComponent } from "./Carousel/Carousel";
